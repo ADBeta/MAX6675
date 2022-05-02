3409b26af2e57a07aa34317fa9bbb933f5bb3444
Ashley <ashleydylanrudd@gmail.com>
2022-05-02 10:14:03 +0100

    MAX6675 V2.0.5 source

---
+++ MAX6675/src/MAX6675.cpp
@@ -0,0 +1,124 @@
+#include "Arduino.h"
+#include "MAX6675.h"
+
+void MAX6675::init(uint8_t v_SCK, uint8_t v_CS, uint8_t v_SO) { //Req. SCK CS SO
+	//Set global pinouts
+	_SCK = v_SCK;
+	_CS = v_CS;
+	_SO = v_SO;
+	
+	//Convert arduino pin number to asm
+	hw_SCKasm = digitalPinToBitMask(_SCK);
+	hw_CSasm = digitalPinToBitMask(_CS);
+	hw_SOasm = digitalPinToBitMask(_SO);
+	
+	//Allocate ports for each pin
+	hw_SCKport = portOutputRegister(digitalPinToPort(_SCK));
+	hw_CSport = portOutputRegister(digitalPinToPort(_CS));
+	hw_SOport = portOutputRegister(digitalPinToPort(_SO));
+	
+	pinMode(_SCK, OUTPUT);
+	pinMode(_CS, OUTPUT);
+	pinMode(_SO, INPUT);
+	
+	//Put chip into sample & wait mode
+	digitalWrite(_SCK, LOW);
+	digitalWrite(_CS, HIGH);
+}
+
+/*
+	digitalRead, shift original value in one operation: 220 us
+	shift data each step, then or in SOBit: 140us
+	shift SOBit but cbit then or with data: 180us
+	
+*/
+uint16_t MAX6675::getData() {
+	//Converted from arduino to port maniuplation
+	uint16_t datastream = 0; //raw 16 bit data
+	bool SOBit; //Bit detected from input line.
+	
+	/* Read data from MAX6675 */
+	*hw_CSport &= ~hw_CSasm; //CS LOW - Start communication
+	
+	for(uint8_t cbit = 0; cbit < 16; cbit++) {
+		*hw_SCKport &= ~hw_SCKasm; //Falling edge of clock
+		
+		//Shift datastream by 1 to get next bit
+		datastream = datastream << 1;
+		//Get state of SO pin
+		SOBit = digitalRead(_SO);
+		//OR in the read bit.
+		datastream |= SOBit;
+		
+		*hw_SCKport |= hw_SCKasm; //Rising edge of clock
+		
+		//Delay for a short time if flag is set. Mostly not needed.
+		if(SAFETY_DELAY == true) {
+			delayMicroseconds(500); 
+		}
+	}
+	
+	*hw_SCKport &= ~hw_SCKasm; //SCK LOW - Reset SCK pin for next read cycle
+	
+	*hw_CSport |= hw_CSasm; //CS HIGH - Stop communication
+	
+	/* Error checking */
+	//If bit 15 (dummy 0 bit) is 1, error
+	if(datastream >> 15) {
+		return 0;
+	}
+	
+	//If the thermocouple sense bit is 1 TODO
+	if(datastream & 0x04) {
+		return 0;
+	}
+	
+	return datastream;
+}
+
+float MAX6675::getTemp() {
+	uint16_t rawData = getData();
+	
+	if(rawData != 0) {
+		/* Construct temp from data */
+		//Decimal is 0.25 * the first usable 2 bits from data - shifted by 3
+		float decimal = 0.25 * ((rawData >> 3) & 0x03);
+		//temp is bits 14 to 5, 10bits - datastream shifted by 5 - plus decimal
+		float temperature = (rawData >> 5) + decimal;
+		
+		return temperature;
+	}
+	
+	//error condition
+	return -9999.99;
+}
+
+uint16_t MAX6675::getTempFast() {
+	uint16_t rawData = getData();
+	
+	if(rawData != 0) {
+		/* Construct temp from data */
+		//temp is bits 14 to 5, 10bits - datastream shifted by 5
+		uint16_t temperature = (rawData >> 5);
+		
+		return temperature;
+	}
+	
+	//error condition
+	return 9999;
+}
+
+float MAX6675::getTempF() {
+	float temperature = getTemp(); //Get temp in C
+	
+	//If getTemp had errored, error again
+	if(temperature == -9999.99) {
+		return temperature;
+	}
+	
+	//The most common method of of C to F is divide by 5, multiply by 9, add 32
+	//To speed this up into one calculation, multiply by 1.8, add 32
+	temperature = (temperature * 1.8) + 32;
+	
+	return temperature;
+}
---
+++ MAX6675/src/MAX6675.h
@@ -0,0 +1,47 @@
+/*
+ *	Simple MAX6675 Thermocouple IC library. Contains Fahrenheit conversion as 
+ *	well as a fast function that doesn't work with floats. ideal for low power
+ *	ICs like ATTinys'. 
+ *
+ *	Version 2.0.5
+ *	ADBeta 01 May 2022
+ 
+	Notes:
+		CS > LOW to start read
+		Bit 15 is already present at SO but is a dummy, always 0
+		16 clock pulses are required to get a full transmission 
+		SCK > Read SO at falling edge
+		D14 - D3 converted data
+		D2 Open state of thermocouple - 1 = open 0 = closed
+		D1 is LOW
+		D0 is tri-state 
+*/
+#include "Arduino.h"
+
+#ifndef MAX6675_h
+#define MAX6675_h
+
+class MAX6675 {
+	public:
+	
+	bool SAFETY_DELAY = false;
+	
+	/* Functions */
+	void init(uint8_t, uint8_t, uint8_t); //Initialises the sensor. SCK CS SO	
+	
+	uint16_t getData(); //Gets raw 16bit data from the MAX6675
+	
+	float getTemp(); //Uses data and returns temp in C, including decimal
+
+	uint16_t getTempFast(); //Uses data and returns temp in C as int 
+	
+	float getTempF(); //Gets temperature in Celcius, converts to Fahrenheit
+		
+	private:
+	uint8_t _SCK, _CS, _SO; //Arduino pinout
+	//Convert arduino pin number to asm
+	uint8_t hw_SCKasm, hw_CSasm, hw_SOasm;
+	//Allocate ports for each pin
+	volatile uint8_t *hw_SCKport, *hw_CSport, *hw_SOport;
+};
+#endif
